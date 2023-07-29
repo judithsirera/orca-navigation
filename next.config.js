62f8ce3c262623131fdcfa7a4c50c74fe902f5ce
Judith Sirera <judith@acamp.com>
2023-07-29 14:52:22 +0200

    fix next config

--- next.config.js
+++ next.config.js
@@ -3,23 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    unoptimized: true,
+    unoptimized: true
   },
   env: {
     API_URL: process.env.API_URL,
     API_KEY: process.env.API_KEY,
-    MAPBOX_TOKEN: process.env.MAPBOX_TOKEN,
+    MAPBOX_TOKEN: process.env.MAPBOX_TOKEN
   },
-  assetPrefix: './',
-  async redirects() {
-    return [
-      {
-        source: '/.well-known/apple-app-site-association',
-        destination: '/.well-known/apple-app-site-association.json',
-        permanent: false,
-      },
-    ];
-  },
+  assetPrefix: './'
 };
 
 module.exports = nextConfig;
